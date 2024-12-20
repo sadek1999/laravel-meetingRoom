9bddada8814a0d34ce15028e96e7fffcaf97e0d2
sadek1999 <sadekali1530@gmail.com>
2024-12-20 22:39:57 +0600

    create Bookings,Room,Host model controller and view files

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +25,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/booking',BookingController::class);
+    Route::resource('/room',RoomController::class);
+    // Route::resource('/user',UserController::class);
 });
 
 require __DIR__.'/auth.php';
